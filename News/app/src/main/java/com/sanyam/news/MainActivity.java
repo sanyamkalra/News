50efeb00e12f55382b8722fe4dead9f7aebd9394
Sanyam Kalra <sanyamkalra221@gmail.com>
2017-08-04 23:29:42 +0530

    updated

--- News/app/src/main/java/com/sanyam/news/MainActivity.java
+++ News/app/src/main/java/com/sanyam/news/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    //lola
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
